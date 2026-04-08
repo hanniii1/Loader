a7ca2049e5b3553a16bde7f41c289617b6b118bb
hanniii1 <nirvsshop@gmail.com>
2026-04-08 15:21:03 +0800

    Update BFLoader.lua

--- BFLoader.lua
+++ BFLoader.lua
@@ -48,6 +48,7 @@ local routes = {
     [7798947148] = { "Anime Final Quest", "https://api.luarmor.net/files/v4/loaders/992facee57df35654157a7d040aa16fd.lua" },
     [77393318863643] = { "Aura Ascension Ahh game", "https://api.jnkie.com/api/v1/luascripts/public/6a5f9cea79d13310ce5e282d6b13a4ea81c63b565a488719070775678cffc6a2/download" },
     [105626692504093] = { "Be a Brainrot", "https://api.luarmor.net/files/v4/loaders/2467a31cffe47ea62651b0f9197a8100.lua" },
+    [112259901434347] = { "+1 Speed be a Lucky Block!", "https://api.luarmor.net/files/v4/loaders/d0f4c13d54d518c47c4e06d2fe8ba534.lua"},
 }
 local route = routes[game.PlaceId] or routes[game.GameId]
 if not route then
