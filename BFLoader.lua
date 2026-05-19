c62001ffd5ece614e6ec01739b837bed3c964b55
hanniii1 <nirvsshop@gmail.com>
2026-05-19 23:18:22 +0800

    Update BFLoader.lua

--- BFLoader.lua
+++ BFLoader.lua
@@ -40,7 +40,6 @@ local function setKey(key)
 end
 local routes = {
     [114640202062357] = { "Swing Obby For Brainrots", "https://api.luarmor.net/files/v4/loaders/99d5f951b48d089c55f94310cce9276e.lua" },
-    [130594398886540] = { "Garden Horizons", "https://api.luarmor.net/files/v4/loaders/069acf492628081651416f39de947b77.lua" },
     [7856269159] = { "Anime Overload", "https://api.luarmor.net/files/v4/loaders/e6153c73e2d96eb2d2d95cc9eb9bd94b.lua" },
     [97365843755210] = { "Cut Grass For Brainrots", "https://api.luarmor.net/files/v4/loaders/52f46b7e492243b23953d2c7611e1c44.lua" },
     [124473577469410] = { "Be a Lucky Block", "https://api.luarmor.net/files/v4/loaders/eb83fa1cc2d982f694566e45ad865313.lua" },
@@ -62,6 +61,7 @@ local routes = {
     [10016841656] = { "Noob Tower Defense", "https://api.luarmor.net/files/v4/loaders/b83acbbb777878cbf813d68fb39ca95e.lua"},
     [6409513651] = { "Anime Warrior III", "https://api.luarmor.net/files/v4/loaders/7c5ad6d6d9907dc52339b6992c69929f.lua"},
     [10039338037] = { "Build A Ring Farm", "https://api.luarmor.net/files/v4/loaders/0c4926532de4a165261afd5cdef461e8.lua"},
+    [9348272796] = { "SZA", "https://api.luarmor.net/files/v4/loaders/d3544bfeec41edddd5cc82b4719bebae.lua"},
 }
 local route = routes[game.PlaceId] or routes[game.GameId]
 if not route then
