478e6c7bba91d77f686cc5d9983e3227a550c51c
hanniii1 <nirvsshop@gmail.com>
2026-07-13 17:52:06 +1000

    Update BFLoader.lua

--- BFLoader.lua
+++ BFLoader.lua
@@ -71,6 +71,7 @@ local routes = {
     [8356066619] = { "Anime Squadron", "https://api.luarmor.net/files/v4/loaders/96c0cb77113aab1b03c99efd25e8b0a9.lua"},
     [10200395747] = { "Grow a Garden 2", "https://api.luarmor.net/files/v4/loaders/06c27f35842892f540c47c5b829cde13.lua"},
     [9826885587] = { "Evomon", "https://api.luarmor.net/files/v4/loaders/1840c1cc0b9aa949a220c8527299cc3b.lua"},
+    [10204207151] = { "Catch a Brainrot", "https://api.luarmor.net/files/v4/loaders/c9199c243c613cae48ca8a4299d3cbf7.lua"},
 }
 local route = routes[game.PlaceId] or routes[game.GameId]
 if not route then
